07e4b618599316581fd694e59de9465e541a789b
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-26 22:45:34 +0530

    Draw a single branch (the trunk)

--- src/main.c
+++ src/main.c
@@ -2,8 +2,19 @@
 #include <raylib.h>
 #include <math.h>
 
+// --- CONSTANTS ------------>
+
 #define WIDTH 900
 #define HEIGHT 600
+
+#define BRANCH_COLOR BLACK
+#define INITIAL_THICKNESS 20
+
+// --- PROTOTYPES ------------>
+
+void DrawBranch(float, float, float, float, float);
+
+// --- ENTRY POINT ------------>
 
 int main(void)
 {
@@ -15,10 +26,21 @@ int main(void)
 	{
 		BeginDrawing();
 		ClearBackground(WHITE);
+		DrawBranch(WIDTH / 2, HEIGHT - 20, 300, 0, INITIAL_THICKNESS);
 		EndDrawing();
 	}
 
 	CloseWindow();
 
 	return 0;
+}
+
+// --- IMPLEMENTATIONS ------------>
+
+void DrawBranch(float x, float y, float length, float angle, float thickness)
+{
+	Vector2 start = {x, y};
+	Vector2 end = {x, y - length};
+
+	DrawLineEx(start, end, thickness, BRANCH_COLOR);
 }
