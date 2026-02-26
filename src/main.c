77b1f09324a02c16cff333eb363c433cce842ab7
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-26 22:29:58 +0530

    Basic Raylib setup

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <raylib.h>
+#include <math.h>
 
+#define WIDTH 900
+#define HEIGHT 600
 
 int main(void)
 {
-	printf("Hello World!\n");
+	InitWindow(WIDTH, HEIGHT, "Fractal Tree");
+
+	SetTargetFPS(1);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(WHITE);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
